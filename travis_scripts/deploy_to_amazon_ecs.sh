7504d7a5b21717c05fd5b20ba00ea2bce21b6729
zhangxue <xue_yuan.zhang@nokia-sbell.com>
2021-03-22 20:27:30 +0800

    fix ecs-cli configure profile command

--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -1,5 +1,5 @@
 echo "Launching $BUILD_NAME IN AMAZON ECS"
-ecs-cli configure profile --profile-name eagle-eye-aws-dev ---access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY
+ecs-cli configure profile default --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY
 ecs-cli configure --region us-east-2c --cluster spmia-tmx-dev
 ecs-cli compose --file docker/common/docker-compose.yml up
 rm -rf ~/.ecs
