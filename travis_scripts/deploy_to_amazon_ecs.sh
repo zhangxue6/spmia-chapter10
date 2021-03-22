b65f9d3e8b978adc7aa2765a168a6f07b3cdbe19
zhangxue <xue_yuan.zhang@nokia-sbell.com>
2021-03-22 19:40:38 +0800

    changes to match my AWS settings

--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -1,4 +1,4 @@
 echo "Launching $BUILD_NAME IN AMAZON ECS"
-ecs-cli configure --region us-west-1 --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY --cluster spmia-tmx-dev
+ecs-cli configure --region us-east-2c --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY --cluster spmia-tmx-dev
 ecs-cli compose --file docker/common/docker-compose.yml up
 rm -rf ~/.ecs
