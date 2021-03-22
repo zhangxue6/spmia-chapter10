0cc767eb3be560769ed7cc61f7c3014aaddfed98
zhangxue <xue_yuan.zhang@nokia-sbell.com>
2021-03-22 23:56:57 +0800

    change AWS keys

--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -1,5 +1,5 @@
 echo "Launching $BUILD_NAME IN AMAZON ECS"
-ecs-cli configure profile --profile-name eagle-eye-aws-dev-profile --access-key AKIA4JVP6YTE2LLEIMYX --secret-key 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
+ecs-cli configure profile --profile-name eagle-eye-aws-dev-profile --access-key AKIA4JVP6YTEYOFJQZEI --secret-key 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
 ecs-cli configure --region us-east-2 --cluster spmia-tmx-dev --config-name eagle-eye-aws-dev-config
 ecs-cli compose --ecs-profile eagle-eye-aws-dev-profile --file docker/common/docker-compose.yml up
 rm -rf ~/.ecs
