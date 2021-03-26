27e0bab4b160027ffed86996e321f470925d4f5d
zhangxue <xue_yuan.zhang@nokia-sbell.com>
2021-03-26 11:24:38 +0530

    used encrypted environment variables

--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -1,5 +1,5 @@
 echo "Launching $BUILD_NAME IN AMAZON ECS"
-ecs-cli configure profile --profile-name eagle-eye-aws-dev-profile --access-key AKIA4JVP6YTEYOFJQZEI --secret-key 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
+ecs-cli configure profile --profile-name eagle-eye-aws-dev-profile --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY
 ecs-cli configure --region us-east-2 --cluster spmia-tmx-dev --config-name eagle-eye-aws-dev-config
 ecs-cli compose --ecs-profile eagle-eye-aws-dev-profile --file docker/common/docker-compose.yml up
 rm -rf ~/.ecs
